32680e8a74a30cd3500327663ed37f272f9fd6ea
Manas Shukla <manasshukla01304@gmail.com>
2025-09-18 10:09:23 +0530

    First React Program

---
+++ 01viteReact/src/App.jsx
@@ -0,0 +1,17 @@
+import Chai from "./chai"
+
+function App() {
+
+  return (
+
+    <>
+    
+      <h1>Chai aur React with Vite | Manas Shukla</h1>;
+      <Chai/>
+    
+    </>
+    
+  )
+}
+
+export default App
---
+++ 01viteReact/src/chai.jsx
@@ -0,0 +1,11 @@
+
+
+function Chai(){
+
+    return(
+
+        <h3>Chai is ready! You can have it.</h3>
+    )
+}
+
+export default Chai;
