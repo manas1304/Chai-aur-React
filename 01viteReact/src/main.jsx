0cbe693228d15a2f3785b61175ac8e9e4348d933
Manas Shukla <manasshukla01304@gmail.com>
2025-09-18 11:47:09 +0530

    Made my Custom React and understood the Create Element in React

--- 01viteReact/src/main.jsx
+++ 01viteReact/src/main.jsx
@@ -1,9 +1,19 @@
 import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+const reactElement = React.createElement(
+
+    'a',
+    {href: 'https://google.com', target:'_blank'},
+    'Take me to Google'
+
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // <StrictMode>
+  //   <App />
+  // </StrictMode>,
+  reactElement
 )
